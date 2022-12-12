a29aaf4516881a646d2da9c74ad3571351fec73c
Your Name <you@example.com>
2022-12-12 23:57:58 +0530

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+    RWOnly ob = new RWOnly();
+    //ob.name = "Arvind";
+    //'name' has private access in 'com.driver.RWOnly'
+    ob.setName("Arvind");
+    System.out.println(ob.getName()
+    );
+  }
 }
